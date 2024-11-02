1d281ae445318b48e30bd26d350c0cacfce73543
ramandeepkaur-20 <raman.ngh12@gmail.com>
2024-11-02 17:30:49 +0530

    Add files via upload

---
+++ college project/index.js
@@ -0,0 +1,15 @@
+let navbar = document.querySelector(' .navbar');
+document.querySelector('#menu-bar').onclick=() =>{
+    navbar.classlist.toggle('active');
+}
+
+
+
+
+
+let search = document.querySelector(' .search');
+document.querySelector('#search').onclick=() =>{
+    search.classlist.toggle('active');
+}
+
+
